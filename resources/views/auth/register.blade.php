0d5c9ba866717df5e82629c4d6bed380e7c33bec
danrpinho <up201505302@fe.up.pt>
2018-04-15 12:35:52 +0100

    Added A8 pdf

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,43 +1,6 @@
 @extends('layout')
 
 @section('content')
-{{--<form method="POST" action="{{ route('register') }}">--}}
-    {{--{{ csrf_field() }}--}}
-
-    {{--<label for="name">Name</label>--}}
-    {{--<input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>--}}
-    {{--@if ($errors->has('name'))--}}
-      {{--<span class="error">--}}
-          {{--{{ $errors->first('name') }}--}}
-      {{--</span>--}}
-    {{--@endif--}}
-
-    {{--<label for="email">E-Mail Address</label>--}}
-    {{--<input id="email" type="email" name="email" value="{{ old('email') }}" required>--}}
-    {{--@if ($errors->has('email'))--}}
-      {{--<span class="error">--}}
-          {{--{{ $errors->first('email') }}--}}
-      {{--</span>--}}
-    {{--@endif--}}
-
-    {{--<label for="password">Password</label>--}}
-    {{--<input id="password" type="password" name="password" required>--}}
-    {{--@if ($errors->has('password'))--}}
-      {{--<span class="error">--}}
-          {{--{{ $errors->first('password') }}--}}
-      {{--</span>--}}
-    {{--@endif--}}
-
-    {{--<label for="password-confirm">Confirm Password</label>--}}
-    {{--<input id="password-confirm" type="password" name="password_confirmation" required>--}}
-
-    {{--<button type="submit">--}}
-      {{--Register--}}
-    {{--</button>--}}
-    {{--<a class="button button-outline" href="{{ route('login') }}">Login</a>--}}
-{{--</form>--}}
-
-
 <div class="container" style="width: 100%; max-width: 33em;">
 
     <form method="POST" action="{{ route('login') }}" style="background-color: #eee; padding: 20px; padding-top: 10px; border-radius: 10px; border: 1px solid #ccc;">
