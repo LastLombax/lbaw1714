227930b3137cff259f61ef2a330420e47c723a9b
danrpinho <up201505302@fe.up.pt>
2018-04-16 12:20:44 +0100

    Removed other mentions

--- app/User.php
+++ app/User.php
@@ -30,11 +30,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-//    /**
-//     * The cards this user owns.
-//     */
-//     public function cards() {
-//      return $this->hasMany('App\Card');
-//    }
 }
--- routes/web.php
+++ routes/web.php
@@ -45,11 +45,6 @@ Route::get('events/{event}', 'EventController@showEvent')->name('event');
 //Route::post('events/{id}', 'EventController@index')->name('event');
 
 // API
-/*Route::put('api/cards', 'CardController@create');
-//Route::delete('api/cards/{card_id}', 'CardController@delete');
-Route::put('api/cards/{card_id}/', 'ItemController@create');
-Route::post('api/item/{id}', 'ItemController@update');
-Route::delete('api/item/{id}', 'ItemController@delete');*/
 
 // Authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
