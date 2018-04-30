3d3972818548c40e0ebca7bceae13f4b2e0d4f70
Lantaros <ruileixo@hotmail.com>
2018-04-30 20:01:21 +0100

    Fixed var typo

--- resources/views/pages/events/upcomingEventFragment.blade.php
+++ resources/views/pages/events/upcomingEventFragment.blade.php
@@ -13,7 +13,7 @@
 
 
 		echo '<div class="card-body">';
-		echo '<h5 class="card-title" style="font-size: 1.2em">' . $frst->name . '</h5>';
+		echo '<h5 class="card-title" style="font-size: 1.2em">' . $first->name . '</h5>';
 		echo '<h6 class="card-subtitle text-muted">' . $first->startday . '</h6>';
 		echo '</div>';
 		echo '<img style="width: 100%; height: 200px; object-fit: cover;" src="' . Storage::url($first->imagepath) . '" alt="Card image">';
