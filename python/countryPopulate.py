0131aa3aeb581d1eee766bbeef9c30de79052cf6
Lantaros <ruileixo@hotmail.com>
2018-05-01 22:44:58 +0100

    Filled Country Table, changing select inputs throughout the website

--- python/countryPopulate.py
+++ python/countryPopulate.py
@@ -1,21 +1,24 @@
 import os
 
-def loadCountryCodes(map):
+def loadCountryCodes():
     file = open('countryCodes.txt', 'r')
     sqlFile = open('countries.sql', 'w')
 
     file.readline()
     while True:
         str = file.readline()
+        str = str.split(',')[0]
+        #str = str[:-1].title()
+        print(str)
 
         if len(str) == 0:
             break
 
-        print(str)
-        value, key = str.split(',')
-        print(key + "=>" + value)
-        map[key] = value
-        sqlFile.write("INSERT INTO country(name) VALUES('" + value + "');\n")
+        # print(str)
+        # value, key = str.split(',')
+        # print(key + "=>" + value)
+        # map[key] = value
+        sqlFile.write("INSERT INTO country(name) VALUES('" + str + "');\n")
 
     sqlFile.close()
     file.close()
@@ -33,4 +36,5 @@ def countryPopulate():
     return
 
 
-countryPopulate()
+#countryPopulate()
+loadCountryCodes()
--- resources/views/pages/events/editEvent.blade.php
+++ resources/views/pages/events/editEvent.blade.php
@@ -65,7 +65,18 @@
                   <label class="col-form-label" for="country" style="width:100%;">
                     <b>Country</b>
                     <div class="form-inline form-group mb-2" style="width:100%;">
-                      @include('layouts.countrySelector')
+                      <div class="form-group">
+                        <select id="country" name="country" class="form-control" required>
+                          {{--<option value="">Choose</option>--}}
+                            @foreach(App\Country::all() as $country)
+                                {{--@if($event->$country == $country->idcountry)--}}
+                                    {{'<option value="' . $country->name .'" selected>' . $country->name . '"</option>"'}}
+                               {{-- @else
+                                    {{'<option value="' . $country->name .'">' . $country->name . '"</option>"'}}
+                                @endif--}}
+                            @endforeach
+                        </select>
+                      </div>
                     </div>
                   </label>
 
