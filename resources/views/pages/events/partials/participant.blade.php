8f864b2c2d9592abc654417eb9b5e10683bacdcb
Lantaros <ruileixo@hotmail.com>
2018-05-06 12:41:43 +0100

    Solved Memeber::imagePath issue

--- resources/views/pages/events/partials/participant.blade.php
+++ resources/views/pages/events/partials/participant.blade.php
@@ -2,7 +2,7 @@
     <a href="#"
        class="list-group-item list-group-item-action flex-column align-items-start">
         <a href="{{route('profile', $member->username)}}">
-        <img src="{{$member->imagePath}}"
+        <img src="{{$member->imagePath()}}"
              style="width: 20%; height: 100px; object-fit: cover; float:left;">
         </a>
         <h5>{{$member->username}}</h5>
