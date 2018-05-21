1ed8bc0eb1191aa4634b27641f50f76725d0717e
danrpinho <up201505302@fe.up.pt>
2018-05-21 12:53:28 +0100

    Fixed password reset css

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,56 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    @section('title')
-        Reset Password
-    @endsection
-    @include('layouts.visitorHead')
-</head>
+    <head>
+        @section('title')
+            Reset Password | ReEvent
+        @endsection
+        @include('layouts.visitorHead')
+    </head>
     <body>
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+    <div class="container" style="width: 100%; max-width: 400px; margin: auto; padding-top: 23px;">
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="margin: 0 auto; width: 200px; border-top-left-radius: 10px; border-top-right-radius: 10px;">
+            <a class="navbar-brand" href="/" style="margin: 0 auto;"><img src="/icon/logo.png"> ReEvent</a>
+        </nav>
+
+        <br>
+            <form method="POST" action="{{ route('password.email') }}" style="margin-top:-22px; background-color: #eee; padding: 20px; padding-top: 10px; border-radius: 10px; border: 1px solid #ccc;">
+                {{ csrf_field() }}
+                <fieldset>
+                    <h3>Reset Password</h3>
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email" class="col-form-label">E-Mail Address</label>
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                            @if ($errors->has('email'))
+                                <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+                            @endif
                         </div>
-                    @endif
-
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                </fieldset>
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary">
+                            Send Password Reset Link
+                        </button>
+                    </div>
                 </div>
-            </div>
+            </form>
+            @include('layouts.footer')
         </div>
-    </div>
-</div>
-  </body>
+    </body>
 </html>
-
