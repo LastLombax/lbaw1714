b225ab4483ca1a48b5ec56f559010117429da464
Francisco Silva <francisjssilva@gmail.com>
2018-05-22 23:26:06 +0100

    correction of css and bugs #2

---
+++ app/TicketType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TicketType extends Model
+{
+    protected $primaryKey = 'idtickettype';
+    protected $table = 'tickettype';
+
+}
