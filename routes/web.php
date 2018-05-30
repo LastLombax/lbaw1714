6be06a2f755044e995c58dce60eef240ee119858
LastLombax <up201503447@fe.up.pt>
2018-05-30 01:28:28 +0100

    started admin

--- routes/web.php
+++ routes/web.php
@@ -118,7 +118,12 @@ Route::group(['middleware' => 'App\Http\Middleware\MemberMiddleware'], function(
         Route::get('communities/{community}/edit', 'CommunityController@editForm')->name('editCommunity')->where('community', '[0-9]+');
         Route::patch('communities/{community}/edit', 'CommunityController@edit')->name('editCommunity');
         Route::delete('communities/{community}', 'CommunityController@delete')->name('deleteCommunity');
-  });
+    });
+
+      //Administration
+     Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function(){
+        Route::get('administration', 'AdminController@index')->name('administration');
+    });
 });
 
 
